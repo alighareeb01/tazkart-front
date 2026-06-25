10eb3fb64603c872b0a44343ef4d5f99ad4a9fc8
alighareeb01 <ghareeb874@gmail.com>
2026-06-25 16:06:14 +0300

    fix sth in app

--- src/Components/RessetPassword/ResetPassword.jsx
+++ src/Components/RessetPassword/ResetPassword.jsx
@@ -141,12 +141,11 @@ export default function ResetPassword() {
 
           <div>
             <button
-              //   disabled={loading}
+              disabled={loading}
               type="submit"
               className="disabled:hover:cursor-not-allowed flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             >
-              {/* {loading ? "loading" : " Login"} */}
-              Enter Your Email please
+              {loading ? "Reset successfully" : " Reset Your Password"}
             </button>
           </div>
         </form>
