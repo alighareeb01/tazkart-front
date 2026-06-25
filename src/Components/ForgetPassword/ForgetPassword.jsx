46b95a3a7e2bc5bb5b4b86e544c97c32f3ba3f80
alighareeb01 <ghareeb874@gmail.com>
2026-06-25 16:03:30 +0300

    fix

--- src/Components/ForgetPassword/ForgetPassword.jsx
+++ src/Components/ForgetPassword/ForgetPassword.jsx
@@ -109,8 +109,7 @@ export default function ForgetPassword() {
               type="submit"
               className="disabled:hover:cursor-not-allowed flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             >
-              {/* {loading ? "loading" : " Login"} */}
-              Enter Your Email please
+              {loading ? "sent" : "  Enter Your Email please"}
             </button>
           </div>
         </form>
