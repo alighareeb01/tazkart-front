89524a70b427645195aae6fdcd84c7e46cda71fe
alighareeb01 <ghareeb874@gmail.com>
2026-06-25 12:59:46 +0300

    added logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -54,7 +54,7 @@ export default function Navbar() {
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex shrink-0 items-center">
                 <img
-                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                  src="images.jpg"
                   alt="Your Company"
                   className="h-8 w-auto"
                 />
