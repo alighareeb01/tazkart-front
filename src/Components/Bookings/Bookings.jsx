7b796920abe33855f3b375e396587bf8ea03933c
alighareeb01 <ghareeb874@gmail.com>
2026-06-27 20:21:15 +0300

    fix

--- src/Components/Bookings/Bookings.jsx
+++ src/Components/Bookings/Bookings.jsx
@@ -56,7 +56,7 @@ export default function Bookings() {
   ) : err ? (
     <Errorfetching />
   ) : (
-    <div className="min-h-screen bg-gray-800 mt-20">
+    <div className="min-h-screen bg-gray-800 ">
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 p-5 px-6">
         {data?.length ? (
           data.map((el, index) => (
